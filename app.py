71215fb9c330316c56a566c00090aabc9b54ff92
Charan H U <charanhumail@gmail.com>
2021-09-07 14:14:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import altair as alt
+import plotly.figure_factory as ff
 
 st.title('Uber pickups in NYC, by Charana H U')
 
