b4b6009f14a33fee3497180fc6c6578ee7b4f78c
Charan H U <charanhumail@gmail.com>
2021-09-07 14:12:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,3 +34,41 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+
+st.title("Histogram")
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+st.pyplot(fig)
+
+st.title("Line Chart")
+chart_data = pd.DataFrame(np.random.randn(40, 3), columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+
+st.title("Area_Chart")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+st.area_chart(chart_data)
+
+st.title("Altair_chart")
+df = pd.DataFrame(np.random.randn(200, 3), columns=['a', 'b', 'c'])
+c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.altair_chart(c, use_container_width=True)
+
+
+
+# Add histogram data
+
+st.title("plotly_chart")
+
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+st.plotly_chart(fig, use_container_width=True)
